ede78340334cdc7e0ce4ba6717f3a7b829342818
harikaparikala@gmail.com <harikaparikala@gmail.com>
2026-07-23 12:32:49 +0530

    day1

---
+++ main.py
@@ -0,0 +1,63 @@
+#we use 
+'''print("harika\n"*3)
+#we can use doubble \\ for it print as it is
+print("c:\\new\\test")
+#raw string
+print(r"c:\new\test")'''
+#usev of +,*
+'''s='coffie'
+t='tee'
+print(s+t)
+u=3
+#print(u+t)#error
+print(u*t)
+print("harika","ammu",sep=" ")
+print("hel")
+#print(int("1.2"))# error
+print(int(float("1.2")))
+#boolean statment
+a=""
+print(bool(a))'''
+#write code to find even and odd numbers
+'''n=int(input("enter the number:"))
+if n%2==0:
+    print("even")
+else:
+    print("odd")'''
+
+dict={
+    "name":"sriram",
+    "gen":"male",
+    "age":21,
+    "courseS":["CSM","CSD","CSE"]
+}
+print(dict)
+#chage sriram to harika
+'''dict['name']="harika"
+dict['age']=20
+dict['gen']="FM"
+print(dict)'''
+'''dict.update({"name":"harika"})
+print(dict)
+print(dict.keys())
+print(dict.items())
+print(dict.values())
+print(dict["age"])
+print(dict.get("name")
+l=[1,2,3,4,5,6]
+l.remove(3)
+print(l)
+l.pop()
+print(l)
+l.pop(3)
+print(l)
+l.append(8)
+print(l)'''
+l=[1,2,3]
+x=[]
+print(len(x))
+x.append(l)
+print(x)
+print(len(x),"len(x)")
+print(len(x[0]),"len(x[0])")
+m=[4,5,6]
